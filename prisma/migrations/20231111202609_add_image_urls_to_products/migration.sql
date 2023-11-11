1f3e768413647f8f131af46a3300c99104d2c870
Paulo Ricardo <paulofsa98@gmail.com>
2023-11-11 17:32:10 -0300

    feat: add seeds script to prisma and imageIrls to Product table

--- prisma/migrations/20231111202609_add_image_urls_to_products/migration.sql
+++ prisma/migrations/20231111202609_add_image_urls_to_products/migration.sql
@@ -1,3 +1,13 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `imageUrl` on the `Product` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Product" DROP COLUMN "imageUrl",
+ADD COLUMN     "imageUrls" TEXT[];
+
 -- CreateTable
 CREATE TABLE "Account" (
     "id" TEXT NOT NULL,
